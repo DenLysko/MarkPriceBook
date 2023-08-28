57c80039b6f81b9739780ad805e13b60ad343000
DenLysko <denislysko@yandex.ru>
2023-08-28 22:48:15 +0300

    feat: class and connection for db

---
+++ Mark Price's Book/Chapter10/WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Console;
+
+namespace Packt.Shared;
+public class Northwind : DbContext
+{
+    public Northwind()
+    {
+        OnConfiguring(new DbContextOptionsBuilder());
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connection = "DataSource=.;" +
+            "Initial Catalog=Northwind;" +
+            "Integrated Security=true;"; 
+            //"MultipleActiveResultSets=true;";
+
+        optionsBuilder.UseSqlServer(connection);
+
+    }
+}
