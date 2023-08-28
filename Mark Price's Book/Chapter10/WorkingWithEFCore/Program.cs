65eeb4e8309a340b282eb357f372fe2d18e52651
DenLysko <denislysko@yandex.ru>
2023-08-28 22:49:25 +0300

    feat: checking throw Console and creating connection

--- Mark Price's Book/Chapter10/WorkingWithEFCore/Program.cs
+++ Mark Price's Book/Chapter10/WorkingWithEFCore/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared;
+using static System.Console;
+
+namespace WorkingWithEFCore;
+
+public class Program
+{
+    public static void Main()
+    {
+        WriteLine($"Using {ProjectConstants.DatabaseProvider} database provider.");
+        var db = new Northwind();
+    }
+}
